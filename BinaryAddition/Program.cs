655a7a6583dc7972ea6d0c0b390c4fc38a12791c
Кирилл Шемет <schemet.k.666@mail.ru>
2022-06-14 00:22:16 +0300

    new Task

---
+++ BinaryAddition/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+int a = 2;
+int b = 1;
+string c = Kata.AddBinary(a, b);
+Console.WriteLine(c);
+
+public static class Kata
+{
+    public static string AddBinary(int a, int b)
+    {
+        return Convert.ToString(a + b, 2);
+    }
+}
