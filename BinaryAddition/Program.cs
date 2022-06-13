959667b602982ae0859006cfbb5dba4cb37f1dfe
ShemetKirill <75487883+ShemetKirill@users.noreply.github.com>
2022-06-14 00:21:11 +0300

    new Task(1)

---
+++ BinaryAddition/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 2;
+int b = 1;
+string c = Kata.AddBinary(a, b);
+Console.WriteLine(c);
+
+public static class Kata
+{
+    public static string AddBinary(int a, int b)
+    {
+        return Convert.ToString(a + b, 2);
+    }
+}
---
+++ BuildTower/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using CodeWars;
+
+Kata kata=new Kata();
+List<string> sList = kata.wave("dg  k /dfgh");
+foreach (var VARIABLE in sList)
+{
+    Console.WriteLine(VARIABLE);
+}
+
+namespace CodeWars
+{
+    public class Kata
+    {
+        public List<string> wave(string str)
+        {
+            List<string> resultList=new List<string>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                char[] ch = str.ToCharArray();
+                ch[i]=Char.ToUpper(ch[i]);
+                    resultList.Add(new string(ch));
+            }
+            return resultList;
+        }
+    }
+}
